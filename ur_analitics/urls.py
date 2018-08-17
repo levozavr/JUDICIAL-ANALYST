fb3fde9085d1514209357d9ffe83a3dbc6b8616c
korwin <levozavr@mail.ru>
2018-08-17 20:59:04 +0300

    small repairs + add logging into file

--- ur_analitics/urls.py
+++ ur_analitics/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'', include('analitics.urls')),
+    url(r'^', include('analitics.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
               static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

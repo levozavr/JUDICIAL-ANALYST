df7021f1496193396a61e7cd8a533d4de6509219
korwin <levozavr@mail.ru>
2018-08-24 18:44:23 +0300

    new logic of demonstration search results

--- analitics/utils_backend.py
+++ analitics/utils_backend.py
@@ -6,7 +6,7 @@ from analitics.forms import DocumentForm
 from analitics.parser.main import parse
 import analitics.parser.main as magic
 from datetime import datetime
-from analitics.finder.main import searcher
+from analitics.finder.main import searcher_sols, searcher_links
 import os
 
 
@@ -48,33 +48,42 @@ def upload(request):
         return HttpResponseRedirect('/')
 
 
-
-def give_links(request):
+def give_sols(request):
     try:
         search_str = request.GET['link']
-        links = searcher(search_str)
-        print(links)
+        sols = searcher_sols(search_str)
     except Exception:
         return HttpResponse(f"[ERROR {datetime.now()}]: Please don't use api with out interface")
     answer = []
-    for link in links:
-        ans = {'href': f"/result?doc_name={link['place']['doc_name']}&sol_num={link['place']['sol_num']}"}
-        ans.update({'text': link['text']})
+    for sol in sols:
+        ans = {'href': f"/result?doc_name={sol['doc_name']}&sol_num={sol['number']}&link={search_str}",
+               'text': sol['name']}
         answer.append(ans)
-    return render(request, 'analitics/result.html', {'links': answer})
+    return render(request, 'analitics/result.html', {'sols': answer})
 
 
-def give_solutions(request):
-    try:
+def give_text(request):
+    if 1:
         sol_num = request.GET['sol_num']
         doc_name = request.GET['doc_name']
+        links = searcher_links(request.GET['link'], sol_num, doc_name)
+        print(links)
         for doc in magic.documents:
             if doc['name'] == doc_name:
                 ans = []
-                for line in doc['solutions'][int(sol_num)]['lines']:
-                    ans.append(line['text'])
+                for num_line, line in enumerate(doc['solutions'][int(sol_num)]['lines']):
+                    for num, word in enumerate(line['text'].split(' ')):
+                        color_start = None
+                        color_stop = None
+                        for link in links:
+                            if link['num_line'] == num_line and link['begin'] == num:
+                                color_start = 1
+                            if link['num_line'] == num_line and link['end'] == num:
+                                color_stop = 1
+                        ans.append({"word": word, "color_start": color_start, 'color_end':color_stop, "end": None})
+                    ans.append({"word": None, "color": None, "end": 1})
                 return render(request, 'analitics/solution.html',
-                              {'text': ans, 'name': doc['solutions'][int(sol_num)]['name']})
+                              {'text': ans, 'links': links, 'name': doc['solutions'][int(sol_num)]['name']})
 
-    except Exception:
+    else:
         return HttpResponse(f"[ERROR {datetime.now()}]: Please don't use api with out interface")
